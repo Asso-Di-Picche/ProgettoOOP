29e6adc4437cdf1fe5562e90fd463842554f4d4f
Kevin <kevincela97@gmail.com>
2019-07-01 18:02:17 +0200

    Add stats

--- src/main/java/com/project/OOP/AgricultureAid.java
+++ src/main/java/com/project/OOP/AgricultureAid.java
@@ -2,6 +2,7 @@ package com.project.OOP;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class AgricultureAid {
 
@@ -35,5 +36,41 @@ public class AgricultureAid {
 
     public HashMap<Integer, Float> getObj() {
         return obj;
+    }
+
+    public float getAvg() {
+        int count = 0;
+        float sum = 0;
+        for (Map.Entry<Integer, Float> val : obj.entrySet()) {
+            sum += val.getValue();
+            count++;
+        }
+        return sum/count;
+    }
+
+    public float getMax() {
+        float currentMax = 0;
+        for (Map.Entry<Integer, Float> val : obj.entrySet()) {
+            if(val.getValue() >= currentMax)
+                currentMax = val.getValue();
+        }
+        return  currentMax;
+    }
+
+    public float getMin() {
+        float currentMin = 10000;
+        for (Map.Entry<Integer, Float> val : obj.entrySet()) {
+            if(val.getValue() <= currentMin)
+                currentMin = val.getValue();
+        }
+        return  currentMin;
+    }
+
+    public float getSum() {
+        float sum = 0;
+        for (Map.Entry<Integer, Float> val : obj.entrySet()) {
+            sum += val.getValue();
+        }
+        return sum;
     }
 }
--- src/main/java/com/project/OOP/BaseController.java
+++ src/main/java/com/project/OOP/BaseController.java
@@ -6,22 +6,53 @@ import com.project.OOP.utils.CSVParser;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
 public class BaseController {
-    @RequestMapping(value = "/obj", method = RequestMethod.GET, produces="application/json")
-    String test(){
+    @RequestMapping(value = "/data", method = RequestMethod.GET, produces="application/json")
+    String getAllData(){
         try {
-            JSONObject obj = CSVDownloader.getJSONFromURL("http://data.europa.eu/euodp/data/api/3/action/package_show?id=nywhJT0h8NIfj3nWwXgjPw");
-            CSVDownloader.downloadCSVfromJSON(obj);
             ArrayList<AgricultureAid> objects = CSVParser.getDataFromCSV();
             ObjectMapper mapper = new ObjectMapper();
             return mapper.writeValueAsString(objects);
         } catch (IOException e){
+            return e.toString();
+        }
+    }
+
+    @RequestMapping(value = "/stats", method = RequestMethod.GET, produces="application/json")
+    String getStats(@RequestParam String Geo){
+        try {
+            ArrayList<AgricultureAid> objects = CSVParser.getDataFromCSV();
+            AgricultureAid foundObj = objects.stream().filter(el -> el.getGeo().equals(Geo)).findFirst().orElse(null);
+            HashMap<String, String> result = new HashMap<>();
+            if(foundObj != null){
+                result.put("geo", foundObj.getGeo());
+                result.put("avg", Float.toString(foundObj.getAvg()));
+                result.put("min", Float.toString(foundObj.getMin()));
+                result.put("max", Float.toString(foundObj.getMax()));
+                result.put("sum", Float.toString(foundObj.getSum()));
+            } else {
+                result.put("error", "Element not found!");
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(result);
+        } catch (IOException e){
+            return e.toString();
+        }
+    }
+
+    @RequestMapping(value = "/metadata", method = RequestMethod.GET, produces="application/json")
+    String getMetadata(){
+        try {
+            return "TODO";
+        } catch (Exception e){
             return e.toString();
         }
     }
